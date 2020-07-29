00d419e41ee40cc6b4f93fe01c0055c87c3c5d0b
Andreferraz20 <luizlopesferraz22@gmail.com>
2020-07-29 17:12:43 -0300

    Half of the second class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './components/Pages/Home';
+import CadastroVideo from './components/Pages/Cadastro/Video';
+
+const Pagina404 = _ => (<div>Página 404</div>);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact/>
+      <Route component={ Pagina404 } />
+    </Switch>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
